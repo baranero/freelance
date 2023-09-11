09f60bcfe10cee1c99e3618b2daf244a461d8c3b
baranero <jakub.baran2@gmail.com>
2023-09-11 11:07:56 +0200

    contact form

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -63,7 +63,7 @@ const Contact = () => {
               exit="hidden"
               className="flex-1 flex flex-col gap-6 w-full mx-auto"
               onSubmit={handleSubmit}
-              name="contact"
+              name="contacts"
               method="POST"
               data-netlify="true"
             >
