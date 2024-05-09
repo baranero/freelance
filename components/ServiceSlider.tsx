0764b436717abc2bbe2b8d066646f6dcfc1813ef
baranero <jakub.baran2@gmail.com>
2024-05-09 22:27:41 +0200

    service slider

--- components/ServiceSlider.tsx
+++ components/ServiceSlider.tsx
@@ -58,6 +58,10 @@ const ServiceSlider = () => {
     setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
+  const handleSlideChange = () => {
+    setExpandedIndex(null); // Zwiń opis przy zmianie slajdu
+  };
+
   return (
     <Swiper
       breakpoints={{
@@ -75,31 +79,34 @@ const ServiceSlider = () => {
       }}
       modules={[FreeMode, Pagination]}
       className={`pb-${expandedIndex !== null ? '20' : '10'}`} // Dodaj padding bottom do slidera tylko gdy jakiś element jest rozwinięty
+      onSlideChange={handleSlideChange} // Obsługa zmiany slajdu
     >
       {serviceData.map((item: Service, index: number) => {
         return (
           <SwiperSlide
             key={index}
-            className={`h-full swiper-slide-custom ${
+            className={`h-full bg-[rgba(65,47,123,0.15)] swiper-slide-custom hover:bg-[rgba(89,65,169,0.15)] ${
               expandedIndex === index ? "swiper-slide-expanded" : ""
             }`}
           >
             <div
-              className={`bg-[rgba(65,47,123,0.15)] h-full rounded-lg px-6 py-6 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300`}
+              className={` h-full rounded-lg px-6 pt-6 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer  transition-all duration-300`}
               onClick={() => handleSlideClick(index)}
             >
               <div className="text-4xl text-accent mb-4">{item.icon}</div>
-              <div className="mb-8">
                 <div className="mb-2 text-lg">{item.title}</div>
                 {/* Warunkowe renderowanie opisu */}
-                {expandedIndex === index ? (
-                  <p className="max-w-[350px] leading-normal">{item.description}</p>
-                ) : null}
-              </div>
+
               <div>
                 <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300 mb-20 " />
               </div>
+
             </div>
+            <div className="pb-10">
+                {expandedIndex === index ? (
+                    <p className="leading-normal px-2 text-center">{item.description}</p>
+                  ) : null}
+              </div>
           </SwiperSlide>
         );
       })}
