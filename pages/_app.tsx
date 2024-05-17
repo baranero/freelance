3cf63b83430e7f18ea6f3b83fe8fb46e069749b7
baranero <jakub.baran2@gmail.com>
2024-05-17 13:06:09 +0200

    meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 
 import { appWithTranslation } from "next-i18next";
-import Head from "next/head";
+import Head from "next-head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -21,18 +21,38 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>
-        Jakub Baran | Usługi przeciwpożarowe, instrukcja bezpieczeństwa pożarowego, operat ppoż., projekt, uzgodnienie
+          Usługi przeciwpożarowe, instrukcja bezpieczeństwa pożarowego, operat ppoż., strefy zagrożenia wybuchem, ocena zagrożenia wybuchem, projekt SSP, DSO, oddymianie, oświetlenie awaryjne
         </title>
         <meta
           name="description"
-          content="Inżynieria bezpieczeństwa pożarowego, instrukcje bezpieczeństwa pożarowego, operaty ppoż., audyty, uzgodnienia, systemy sygnalizacji poarowej, dźwiękowe systemy ostrzegawcze, awarayjne oświetlenie ewakuacyjne, systemy oddymiania, oceny zagrożenia przed wybuchem"
+          content="Oferuję usługi inżynierii bezpieczeństwa pożarowego, w tym instrukcje bezpieczeństwa pożarowego, operaty przeciwpożarowe, audyty budynków, projekty systemów sygnalizacji pożarowej, dźwiękowych systemów ostrzegawczych, systemów oddymiania, awaryjnego oświetlenia ewakuacyjnego, wyznaczanie stref zagrożenia wybuchem oraz oceny zagrożenia wybuchem."
         />
+        <meta name="keywords" content="inżynieria bezpieczeństwa pożarowego, instrukcja bezpieczeństwa pożarowego, operat ppoż., systemy sygnalizacji pożarowej, dźwiękowe systemy ostrzegawcze, awaryjne oświetlenie ewakuacyjne, systemy oddymiania, oceny zagrożenia przed wybuchem, audyty przeciwpożarowe" />
+        <meta name="author" content="Jakub Baran" />
+        <meta property="og:title" content="Usługi przeciwpożarowe, instrukcja bezpieczeństwa pożarowego, operat ppoż., strefy zagrożenia wybuchem, ocena zagrożenia wybuchem, projekt SSP, DSO, oddymianie, oświetlenie awaryjne" />
+        <meta property="og:description" content="Oferuję usługi inżynierii bezpieczeństwa pożarowego, w tym instrukcje bezpieczeństwa pożarowego, operaty przeciwpożarowe, audyty budynków, projekty systemów sygnalizacji pożarowej, dźwiękowych systemów ostrzegawczych, systemów oddymiania, awaryjnego oświetlenia ewakuacyjnego, wyznaczanie stref zagrożenia wybuchem oraz oceny zagrożenia wybuchem." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jakubbaran.pl" />
+        <link rel="canonical" href="https://jakubbaran.pl" />
+        <link rel="icon" href="/favicon.ico" />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Jakub Baran",
+            "url": "https://jakubbaran.pl",
+            "description": "Oferuję usługi inżynierii bezpieczeństwa pożarowego, w tym instrukcje bezpieczeństwa pożarowego, operaty przeciwpożarowe, audyty budynków, projekty systemów sygnalizacji pożarowej, dźwiękowych systemów ostrzegawczych, systemów oddymiania, awaryjnego oświetlenia ewakuacyjnego, wyznaczanie stref zagrożenia wybuchem oraz oceny zagrożenia wybuchem.",
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "+48790782993",
+              "contactType": "Customer Service"
+            },
+          })}
+        </script>
       </Head>
       <Layout>
         <AnimatePresence mode="wait">
           <motion.div key={router.route} className="">
-
-
             <Component {...pageProps} />
           </motion.div>
         </AnimatePresence>
