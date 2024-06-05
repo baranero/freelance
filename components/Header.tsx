32396764f3bf51587c032ea768d5fd1982e89f9a
baranero <jakub.baran2@gmail.com>
2024-06-06 00:36:47 +0200

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="">
         <div className="flex mx-auto flex-col text-center justify-center items-center gap-y-2 py-2">
           <Link className="flex items-center" href={"/"}>
-            <Image className="w-14" src={"/logo.jpg"} width={80} height={100} alt="Logo" priority={true} />
+            <Image className="w-14" src={"/logo.jpg"} width={80} height={100} alt="Logo" />
             <h1 className="sm:text-2xl text-lg">
               Fire Protection <span className="text-accent">Solutions</span>
             </h1>
