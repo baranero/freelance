52b3fa70d4d2ab515eea9d500676364d0dd6f36b
baranero <jakub.baran2@gmail.com>
2024-06-08 20:30:43 +0200

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Logo from '../public/Logo.webp'
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       <div className="">
         <div className="flex mx-auto flex-col text-center justify-center items-center gap-y-2 py-2">
           <Link className="flex items-center" href={"/"}>
-            <Image className="w-14" src={"/logo.png"} width={80} height={100} alt="Logo" />
+            <Image className="w-14" src={Logo} width={80} height={100} alt="Logo" />
             <h1 className="sm:text-2xl text-lg">
               Fire Protection <span className="text-accent">Solutions</span>
             </h1>
